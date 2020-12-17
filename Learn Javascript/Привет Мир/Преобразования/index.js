609edd01c5b673e4ffefe6e6d2d2cb458f8410b2
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2020-12-17 18:39:19 +0300

    Add files via upload

---
+++ Learn Javascript/Привет Мир/Преобразования/index.js
@@ -0,0 +1,21 @@
+let value = true;
+
+console.log(typeof value)
+
+value = String(value);
+console.log( typeof value)
+
+// Численное преобразование
+  console.log('6' / '2') // строки преобрахуются в числа
+
+
+
+  let age = Number('Строка');
+
+  console.log(age);
+
+  // Boolean
+
+  alert( Boolean('0'));
+
+  //так как строка не пустая в лней находиться нуль она тру

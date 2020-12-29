3a424a45a1cf8bd3ffc54dd2a7fa0cfe8f1943ab
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2020-12-29 19:58:16 +0300

    Add files via upload

---
+++ Чистый JS/use == практика/Cчетчик/script.js
@@ -0,0 +1,30 @@
+let btnMinus = document.querySelector('.minus' )
+let btnPlus = document.querySelector('.plus')
+let counter = document.querySelector('.counter')
+
+btnMinus.addEventListener('click', () => {
+    counter.innerHTML = a;
+})
+btnPlus.addEventListener('click', () => {
+    counter.innerHTML = b;
+})
+
+function rezult () {
+   let i = 1;
+   return ++i;
+}
+
+let a = rezult();
+
+function rezultPlus () {
+   let i = 1;
+   return --i;
+}
+
+let b = rezultPlus();
+
+
+
+
+
+

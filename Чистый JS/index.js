e4a018fbd912136927641a9ebb8c25ba3f7cd755
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2021-01-29 16:17:40 +0300

    Update index.js

--- Чистый JS/index.js
+++ Чистый JS/index.js
@@ -1,3 +1,37 @@
+яlet pixels = document.querySelectorAll('.pixel')
+let chosenColor = document.querySelector('.chosen-color')
+let eraser = document.querySelector('.eraser')
+
+
+ 
+ chosenColor.onchange = function () {
+      chosenColor.style.backgroundColor =  chosenColor.value
+
+    for (let i of pixels) {
+  
+i.onclick = function() { 
+  console.log(eraser.checked) 
+  if (eraser.checked) {
+    i.style.backgroundColor = 'white' 
+  } else {
+    i.style.backgroundColor = chosenColor.value 
+  }
+  }
+ }
+}  
+
+
+/*
+
+1. Все «пиксели» имеют класс pixel.
+2. Выпадающий список с цветами имеет класс chosen-color.
+3. «Ластик» — это чекбокс с классом eraser.
+4. Когда на «пиксель» кликают, у него должен измениться цвет фона.
+5. Если в момент клика ластик выключен, фон нажатого «пикселя» должен стать того цвета, который выбран в списке.
+6. Если в момент клика ластик включён, фон нажатого «пикселя» должен стать белым — 'white'.
+
+*/
+
 // Прототипы
 const person = {
     name: 'Vlad',

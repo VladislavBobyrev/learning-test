63959d8ea157379e74f89805bfcf37a90f553d28
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2021-01-31 11:11:12 +0300

    Update index.js

--- Чистый JS/index.js
+++ Чистый JS/index.js
@@ -1,4 +1,27 @@
-яlet pixels = document.querySelectorAll('.pixel')
+let euroRate = 74;
+let dollarRate = 63;
+
+let euroAmount = 500;
+let dollarAmount = 2500;
+
+
+
+let rublesAmount = (euroAmount * euroRate) + (dollarAmount * dollarRate)
+
+
+/* Техническое задание
+
+Напиши программу, которая посчитает сколько всего рублей понадобится на поездку.
+
+Курсы валют указаны в переменных euroRate и dollarRate.
+
+Переменные euroAmount и dollarAmount— необходимые суммы на поездку.
+
+Создай переменную rublesAmount и записывай в неё результат вычислений.
+
+*/
+
+let pixels = document.querySelectorAll('.pixel')
 let chosenColor = document.querySelector('.chosen-color')
 let eraser = document.querySelector('.eraser')
 

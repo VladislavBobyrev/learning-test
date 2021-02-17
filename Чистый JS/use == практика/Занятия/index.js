632d6cbdae38304f9d69bf8dfd9089982d647654
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2021-02-17 12:11:36 +0300

    Update index.js

--- Чистый JS/use == практика/Занятия/index.js
+++ Чистый JS/use == практика/Занятия/index.js
@@ -777,3 +777,29 @@ ctx.stroke()
 //ctx.lineWidth = 15
 //ctx.strokeStyle = 'blue'
 //ctx.stroke()
+let key = document.querySelectorAll('.key')
+let display = document.querySelector('.display')
+let clear = document.querySelector('.clear')
+
+
+for(let i of key) {
+  i.onclick = function() {
+  display.textContent += i.textContent
+  
+  }
+}
+
+clear.onclick = function(){
+  display.textContent = ''
+  }
+
+
+
+
+/*
+Кнопки с буквами и «пробел» имеют класс key, а дисплей — класс display.
+
+При клике на кнопку с буквой или пробелом текстовое содержимое этой кнопки должно добавляться к текстовому содержимому дисплея.
+
+Кнопка очистки имеет класс clear. При клике на неё весь текст внутри дисплея должен удаляться, для этого в текстовое содержимое дисплея нужно записать пустую строку.
+*/

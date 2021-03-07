873612583e93f1015f03ebe55047c1a40a905dc4
vladislav <perets1995@gmail.com>
2021-03-07 13:26:58 +0300

    test

---
+++ my-server/server.js
@@ -0,0 +1,21 @@
+//const { createServer } = require('http')
+
+let frend = ['tema', 'vlad',]
+console.log(frend)
+
+let [myFrends, mi,] = frend
+
+console.log(mi)
+
+
+
+let anim = {};
+[anim.cat, anim.dog] = "sneg aria".split(' ');
+
+console.log(anim.cat)
+
+let animal = {};
+[animal.cat, animal.dog] = 'sneg aria'.split(' ');
+
+
+console.log(animal.dog)

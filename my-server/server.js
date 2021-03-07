d0baf9110a001ff07dffa3b233197ed85c986bea
vladislav <perets1995@gmail.com>
2021-03-07 21:45:32 +0300

    server

--- my-server/server.js
+++ my-server/server.js
@@ -7,4 +7,8 @@ const port = 6678
 const server = createServer((_, res) => {
     res.setHeader('Access-Control-Allow-Origin', 'http')
     res.write(JSON.stringify(array))
+    res.end()
 })
+
+server.listen(port)
+console.log(`server is listening ${port} port..`)

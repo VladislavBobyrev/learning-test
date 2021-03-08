86b6141e1e28828e1b125e0c39e12f9c6eba3dd8
vladislav <perets1995@gmail.com>
2021-03-08 15:19:21 +0300

    this

--- Чистый JS/Контекст/main.js
+++ Чистый JS/Контекст/main.js
@@ -1,57 +1,96 @@
+let data = {
+    name: 'valadislav',
+    go() {
+        console.log(this.name)
+    }
+}
 
-//function hello () {
-//    console.log('Hello',this)
-//}
+data.go()
 
-//const person = {
-//    name: 'Vladislav',
-//    age: 15,
-//    sayHello: hello,
-//    sayHelloWindiwd: hello.bind(document),
-//    logInfo: function(job, phone) {
-//        console.group(`${this.name} :info`)
-//        console.log(`Name is ${this.name}`)
-//        console.log(`Age is ${this.age}`)
-//        console.log(`Job is ${job}`)
-//        console.log(`Phone is ${phone}`)
-//        console.groupEnd()
+let calcul = {
 
-//    }
-//}
-
-//const lena = {
-//    name: 'Lena',
-//    age: 12,
-
-//}
-
-// const fnLena = person.logInfo.bind(lena,'frontend', '8-999-343-344-3')
-// fnLena()
-//const fnLena = person.logInfo.call(lena,'frontend', '8-999-343-344-3')
-//fnLena
-
-//person.logInfo.apply(lena, ['frontend', '8-999-343-344-3'])
-
-//// ================
-
-//const array = [1,2,3,4,5]
-
-// function MultBy(arr, n) {
-//     return arr.map(function (i) {
-//         return i * n
-//     })
-// }
-
-// console.log(MultBy(array, 50))
+    read(){
+        this.x = +prompt('zn1')
+        this.y = +prompt('zn2')
+    },
+    sum(){
+        return this.x + this.y
+    },
+    mul(){return this.x * this.y},
+}
+calcul.read()
+calcul.mul()
 
 
-//Array.prototype.MultBy = function(n) {
-//    return this.map( function(i) {
-//        return i * n
-//    })
-//}
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this
+    },
+    down() {
+      this.step--;
+      return this
+    },
+    showStep: function() { // показывает текущую ступеньку
+      alert( this.step );
+      return this
+    }
+  };
 
-//console.log(array.MultBy(2))
+function hello () {
+    console.log('Hello',this)
+}
+
+const person = {
+    name: 'Vladislav',
+    age: 15,
+    sayHello: hello,
+    sayHelloWindiwd: hello.bind(document),
+    logInfo: function(job, phone) {
+        console.group(`${this.name} :info`)
+        console.log(`Name is ${this.name}`)
+        console.log(`Age is ${this.age}`)
+        console.log(`Job is ${job}`)
+        console.log(`Phone is ${phone}`)
+        console.groupEnd()
+
+    }
+}
+
+const lena = {
+    name: 'Lena',
+    age: 12,
+
+}
+
+ const fnLena = person.logInfo.bind(lena,'frontend', '8-999-343-344-3')
+ fnLena()
+const fnLena = person.logInfo.call(lena,'frontend', '8-999-343-344-3')
+fnLena
+
+person.logInfo.apply(lena, ['frontend', '8-999-343-344-3'])
+
+// ================
+
+const array = [1,2,3,4,5]
+
+ function MultBy(arr, n) {
+     return arr.map(function (i) {
+         return i * n
+     })
+ }
+
+ console.log(MultBy(array, 50))
+
+
+Array.prototype.MultBy = function(n) {
+    return this.map( function(i) {
+        return i * n
+    })
+}
+
+console.log(array.MultBy(2))
 
 
 let obj = { 
@@ -95,23 +134,5 @@ let firstName = {
             arrow()
             }
     }
-
-    firstName.hi()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ firstName.hi()
 

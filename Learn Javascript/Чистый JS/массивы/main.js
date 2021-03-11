e91b73bce9f00412708f8bee88abbd5b544cad3c
vladislav <perets1995@gmail.com>
2021-03-11 19:56:11 +0300

    arr

--- Learn Javascript/Чистый JS/массивы/main.js
+++ Learn Javascript/Чистый JS/массивы/main.js
@@ -1,65 +1,47 @@
-let home = ['I', 'go', 'to', 'home']
-console.log( home[1], home.length)
+// split  разделит строку на массив по тому разделителю который мы укозали 
+let userName = 'Vasua, Petya, Masha'
 
-home.splice(2, 2, 'back', 'car') // удали начиная с 2 го элемента 2  эл и вставь back car
-console.log(home)
+let arr = userName.split(', ')
 
-let arr = [ 1, 2, 3, 4, 5]
-let remove = arr.splice(0, 3)
-console.log(remove)
-
-arr.splice(4, 0, 'hello', 'we', 'number')
-console.log(arr)
-
-arr.splice(-1, 0, 'no')
-console.log(arr)
-
-console.log( home.slice( 2, 4))
-
-console.log( arr.concat([6, 7], 8, 9))
-
-let array = [1, 2]
-let arrayLike = {
-    0: -1, 
-    1: 'tesla',
-    [Symbol.isConcatSpreadable]: true,
-    length: 2,
+for(let name of arr){
+    console.log(`hello my name ${name}`)
 }
 
-console.log(array.concat(arrayLike))
+let array = 'Vasua, Petya, Masha'.split(',', 2)
+console.log(array)
 
+let test = 'test'.split('') // split без аргументов разобьет строку на буквы
+console.log(test)
 
-let text = ["Bilbo", "Gandalf", "Nazgul"]
-text.forEach((item, index, array) => {
-   console.log( `My name is ${item}  I,m ${index} my frends ${array}`)
-})
+// join обьедиянет массив в строку через разделитель
+let arrM = ['Вася', 'Петя', 'Маша']
+arrM.join(';')
+console.log(arrM.join('; '))
 
-let myIndexOf = [1, 2, false]
-console.log(myIndexOf.indexOf(false))
-console.log(myIndexOf.indexOf(0))
-console.log(myIndexOf.indexOf(null))
+// reduse
+let num = [1, 2, 3, 4, 5]
+console.log( num.reduce((sum, current) => sum + current, 0)) 
 
-//find  проверяет у каждого обьекта ид и возвращает первое истенное
+let noArr = []
+//console.log( noArr.reduce((sum, current) => sum + current)) // пустой массив вызовет ошибку
+
+let army = {
+    minAge: 18,
+    maxAge: 27,
+    csnLJoin(user){
+        return user.age >= this.minAge && user.age < this.maxAge
+    },
+}
+
 let users = [
-    {id: 1, name: 'vasua'},
-    {id: 2, name: 'sua'},
-    {id: 3, name: 'vsa'},
-    {id: 4, name: 'vua'},
-]
+    {age: 16},
+    {age: 20},
+    {age: 23},
+    {age: 30}
+  ];
 
-let user =users.find(item => item.id == 3)
-console.log(user)
+  let solilder = users.filter(army.csnLJoin, army)
+  console.log(solilder[1])
 
-let somUser = users.filter((item) => item.id < 3)
-console.log(somUser)
-
-let result = text.map(item => item.length)
-console.log(result)
-
-let sorting = [1, 2, 15, 16]
-console.log(sorting.sort( (a, b) => a - b)) //пока не понял что передаем 
- 
-console.log( sorting.reverse() )// обратный порядок
-
-
-
+  //push добавляет элемент в конец массива
+  //pop извоекает  элеме 

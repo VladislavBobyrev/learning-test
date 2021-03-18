187dba949ed467bbb690ca9e706c6e4d6b2d9c54
vladislav <perets1995@gmail.com>
2021-03-18 18:58:12 +0300

    serv

--- my-server/server.js
+++ my-server/server.js
@@ -1,13 +1,15 @@
-const { createServer } = require('http')
+const { createServer, request } = require('http')
 
-const { array } = require('./data')
-const port = 6687
+//const { array } = require('./data')
+const port = 3000
 
-const server = createServer((_, res) => {
-    res.setHeader('Acces-Control-Allow-Origin', 'http://localhost:5500')
-    res.write(JSON.stringify(array))
-    res.end()
+const requairHendler = (request, response) => {
+    console.log(request.url)
+    response.end(`Hello my one Nade.js Server ! ${request.url}`)
+}
+const server = http.createServer(requairHendler)
+
+server.listen(port, (err) => {
+    err ? console.log(`sometaimsing bad happend`, err) :
+    console.log(`Server is listening port ${port}`)
 })
-
-server.listen(port)
-console.log('Node server working...')

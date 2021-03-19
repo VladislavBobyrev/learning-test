0ccbc7a1e63017ea9aba8f66451b0dfee5efa50c
vladislav <perets1995@gmail.com>
2021-03-19 15:33:04 +0300

    job

--- my-server/server.js
+++ my-server/server.js
@@ -1,9 +1,12 @@
-const http = require('http');
-const static = require('node-static');
-const file = new static.Server('.');
+const http = require('http')
+const host = 'localhost'
+const port = 8000
 
-http.createServer((req, res) => {
-  file.serve(req, res);
-}).listen(8080);
-
-console.log('Server running on port 8080');
+const requestListener = (req, res) => {
+    res.writHead(200)
+    res.end('My One server')
+}
+const server = http.createServer(requestListener)
+server.listen(port, host, () => {
+    console.log(`Server is running on  http ${host}:${port}`)
+})

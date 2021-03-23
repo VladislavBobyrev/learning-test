3984e4b22fb23bab3e98eabc7dfbc39dd2741913
vladislav <perets1995@gmail.com>
2021-03-23 11:58:04 +0300

    cnv

--- CSS/convas2D/test/main.js
+++ CSS/convas2D/test/main.js
@@ -10,3 +10,36 @@ let resize = () => {
 resize()
 addEventListener('resize', resize)
 
+let angel = 0
+
+let animation = (obj) => {
+    const { clear, update, render} = obj
+    let timeStamp = 0
+    console.log(clear)
+    console.log(update)
+    console.log(render)
+    requestAnimationFrame(animation)
+
+}
+
+animation({
+     clear(){
+        ctx.clearRect(0, 0, cnv.width, cnv.height)
+     },
+    update(){angel += 1 },
+    render(){
+        ctx.beginPath() 
+        ctx.arc(
+            cnv.width / 2 + 150 * Math.cos(angel),
+            cnv.height / 2 + 150 * Math.sin(angel),
+            5,
+            0,
+            Math.PI * 2,
+            )
+            ctx.fillStyle = 'white'
+            ctx.fill()
+            
+    },
+}
+   
+)

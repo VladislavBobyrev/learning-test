3079df7263aa46de1ec1b7d0f1a1e7762e2e7e05
vladislav <perets1995@gmail.com>
2021-03-23 10:05:02 +0300

    new weakMap

--- Learn Javascript/Чистый JS/weakMap/main.js
+++ Learn Javascript/Чистый JS/weakMap/main.js
@@ -3,3 +3,9 @@ const obj = {}
 weakMap.set(obj, 'this oba in weakMap')
 
 console.log(weakMap)
+
+//console.log( weakMap.set('test', 'snow')) вызовет ошибку тк ключь не обьект а текст
+
+let jon = { name: 'jon', age: 25,}
+let weak = new WeakMap()
+weakMap.set(jon, '...')
--- Learn Javascript/Чистый JS/массивы/main.js
+++ Learn Javascript/Чистый JS/массивы/main.js
@@ -1,37 +1,37 @@
-//let arr =  ["I", "go", "home"]
-//delete arr[1]
-//console.log(arr.length)
-//arr.splice(1, 1, 'runing')
-//console.log(typeof !{})
+let arr =  ["I", "go", "home"]
+delete arr[1]
+console.log(arr.length)
+arr.splice(1, 1, 'runing')
+console.log(typeof !{})
 
-////let remove = arr.splice(0, 2)
-////console.log(remove)
-//arr.splice(2, 0, 'learn JS to', 'I', 'I')
-//console.log(arr)
-//arr.splice(1, 0, 'am')
-//console.log(arr)
-//arr.forEach((item, index, array) => {
-//    console.log(array)
-//})
+//let remove = arr.splice(0, 2)
+//console.log(remove)
+arr.splice(2, 0, 'learn JS to', 'I', 'I')
+console.log(arr)
+arr.splice(1, 0, 'am')
+console.log(arr)
+arr.forEach((item, index, array) => {
+    console.log(array)
+})
 
-//console.log(arr.includes('I'))
+console.log(arr.includes('I'))
 
-//const unique = (arr) => {
-//    let result = []
-//   for(let i of arr){
-//    //   if(!result.includes(i)){
-//    //    result.push(i)
-//    //   }
-//       result.includes(i) ? result : result.push(i)
-//   }
-//    return result
-//}
-//console.log( unique(arr) )
+const unique = (arr) => {
+    let result = []
+   for(let i of arr){
+    //   if(!result.includes(i)){
+    //    result.push(i)
+    //   }
+       result.includes(i) ? result : result.push(i)
+   }
+    return result
+}
+console.log( unique(arr) )
 
-//let map = new Map()
-//console.log(map)
-//map.set('1', 'tesla')
-//console.log(map)
+let map = new Map()
+console.log(map)
+map.set('1', 'tesla')
+console.log(map)
 
 const obj = {
     name: 'valdislav',

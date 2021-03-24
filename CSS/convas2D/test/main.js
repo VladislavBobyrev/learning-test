2e702513125a6137153f9a8b8edd5720ec214d97
vladislav <perets1995@gmail.com>
2021-03-24 12:35:34 +0300

    car

--- CSS/convas2D/test/main.js
+++ CSS/convas2D/test/main.js
@@ -2,27 +2,41 @@ const cnv = document.querySelector('canvas')
 const ctx = cnv.getContext('2d')
 
 let resize = () => {
-    cnv.width = innerWidth
-    cnv.height = innerHeight   
+    cnv.width = 850
+    cnv.height = 800   
 }
 resize()
 addEventListener('resize', resize)
 
 
-const promisImg = new Promise((resolve) => {
+const fonImg = new Promise((resolve) => {
         const img = new Image
         img.src = './img/bg.jpg'
         img.onload = () => resolve(img)
     })
-const image = promisImg.then((data) => data)
+const image = fonImg.then((data) => data)
 
-async function drowGame() { 
+let  drowGame = async() => { 
     requestAnimationFrame(drowGame)   
     const img = await image 
-    const patern = ctx.createPattern(img, 'repeat-x')
+    const patern = ctx.createPattern(img, 'no-repeat')
     ctx.fillStyle = patern
     ctx.fillRect(0, 0, cnv.width, cnv.height)
+    goingCar()
 }
 drowGame()
 
 
+const carImg = new Promise(resolve => {
+    const img = new Image()
+    img.src = './img/car.png'
+    img.onload = () => resolve(img)
+})
+const imgCar = carImg.then(img => img)
+
+let goingCar = async() => {
+    const car = await imgCar
+     let angelX =  
+     let angelY = 
+    ctx.drawImage(car, 160 + angelX, 430 + angelY)    
+}

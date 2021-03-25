d65fbf843fee2cef3d3222aa428783af0d29683d
vladislav <perets1995@gmail.com>
2021-03-25 15:58:41 +0300

    func

--- Learn Javascript/Чистый JS/logoriphm/main.js
+++ Learn Javascript/Чистый JS/logoriphm/main.js
@@ -1,12 +1,15 @@
 const array = [1, 2, 5, 8, 5, 6, 7, 3, 9, 10, 15, 12, 13, 14, 12]
 
+let count = 0
 let lineralSearh = (array, item) => {
-    for(let key of array){
+    for(let key = 0; key < array.length; key++){
+        count += 1
         if(array[key] == item){
-            return key
+            return key           
         }
     }  
     return null  
 }
-let result = lineralSearh(array, 12)
-console.dir(result)
+
+console.dir(`Nomber El :  ${lineralSearh(array, 3)}`)
+console.dir(`amount itteratibl : ${count}`)

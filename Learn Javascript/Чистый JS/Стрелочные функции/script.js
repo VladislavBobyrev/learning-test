db0e5c4af7299cf1ebf58684479ab5734aa904ab
vladislav <perets1995@gmail.com>
2021-03-31 13:01:55 +0300

    fn

--- Learn Javascript/Чистый JS/Стрелочные функции/script.js
+++ Learn Javascript/Чистый JS/Стрелочные функции/script.js
@@ -1,56 +1,60 @@
 
- const a = prompt('число')
- const b = prompt('число')
+// const a = prompt('число')
+// const b = prompt('число')
 
- let strFunc = (a, b) =>  {
-    return a - b
- };
+// let strFunc = (a, b) =>  {
+//    return a - b
+// };
 
- console.log(strFunc(10, 1))
+// console.log(strFunc(10, 1))
 
- let sum = (x, n) => x + n;
- console.log(sum(3,3))
-
-
- const v = (a, b) => a + b * a;
- console.log(v(5,5)) 
+// let sum = (x, n) => x + n;
+// console.log(sum(3,3))
 
 
- let getAge = prompt('Сколько вам лет?');
-
- let str = (getAge > 18) ?
-     () => console.log('hello') :
-     () => console.log('yoooo hello');
- str();
-
- let sum = (a, b) => {
-     let rezult = a + b;
-     return rezult;
- }
-  const x = sum(30, 70);
-  console.log(x)
-
- function asc(question, yes, no) {
-     if (confirm(question)) yes()
-     else no();
- }
- let asc = (qustion, yes, no) => confirm(qustion) ? yes() : no();
-
- asc(
-     'вопрос это первый параметр',
-     () => console.log('я второй параметр ты нажал yes вызвал функцияю true тоесть yes'),
-     () => console.log('я тертий параметр ты нажал отмена тооесть фолс  сработало else вызвал функцию no')
- )
+// const v = (a, b) => a + b * a;
+// console.log(v(5,5)) 
 
 
-let a = 10;
+// let getAge = prompt('Сколько вам лет?');
+
+// let str = (getAge > 18) ?
+//     () => console.log('hello') :
+//     () => console.log('yoooo hello');
+// str();
+
+// let sum = (a, b) => {
+//     let rezult = a + b;
+//     return rezult;
+// }
+//  const x = sum(30, 70);
+//  console.log(x)
+
+// function asc(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+// }
+// let asc = (qustion, yes, no) => confirm(qustion) ? yes() : no();
+
+// asc(
+//     'вопрос это первый параметр',
+//     () => console.log('я второй параметр ты нажал yes вызвал функцияю true тоесть yes'),
+//     () => console.log('я тертий параметр ты нажал отмена тооесть фолс  сработало else вызвал функцию no')
+// )
+
+
+let a = [10]
  
-let str = () => {
-    let a = 14;
-    console.log(a)
-}
+//let str = () => {
+//    let a = 14;
+//    console.log(a)
+//}
 
-str();
-console.log(a)
+//str();
+//console.log(a)
 
-let calcNum = () => a + b
+//let calcNum = () => a + b
+
+
+let a3 = a.map(s => s.length)
+console.dir(a3)

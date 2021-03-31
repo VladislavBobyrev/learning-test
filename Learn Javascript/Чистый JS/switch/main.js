404131f846b15a9e43f33e0b0de2c4107f4336ea
vladislav <perets1995@gmail.com>
2021-03-31 17:12:15 +0300

    try catch throw

--- Learn Javascript/Чистый JS/switch/main.js
+++ Learn Javascript/Чистый JS/switch/main.js
@@ -1,8 +1,36 @@
-switch (key) {  //сравнивае значения если оно равниварианту то выполняет инструкцию
-    case value:
-        // если вэлью то сделай то-то
-        break;
+//switch (key) {  //сравнивае значения если оно равниварианту то выполняет инструкцию
+//    case value:
+//        // если вэлью то сделай то-то
+//        break;
 
-    default:
-        break;
+//    default:
+//        break;
+//}
+
+let res = 0
+//const q = 2
+try {
+    res = 5 / q
+    console.dir(res)
+} catch (error) {       // у еррор есть name massege and stack
+    console.dir(error.name)    
+    console.dir(error.message)    
+    console.dir(error.stack)    
+}
+
+
+let multiply = (a, b) => {
+    if(b == 0){
+        throw new Error("Деление на 0") 
+    }
+    return a / b
+}
+
+let rezult = 0 
+try{
+    res = multiply(3, 0)
+}
+catch(error){
+    console.dir(error.name)
+    console.dir(error.message)
 }

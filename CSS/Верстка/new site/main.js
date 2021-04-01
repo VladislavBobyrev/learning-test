1ee11769435bca1fc4718b4d7cb671b22e5d8a02
vladislav <perets1995@gmail.com>
2021-04-01 11:44:49 +0300

    search

--- CSS/Верстка/new site/main.js
+++ CSS/Верстка/new site/main.js
@@ -1,7 +1,23 @@
-let body = document.querySelector('.page')
-let themeButton = document.querySelector('.theme-button')
+//let body = document.querySelector('.page')
+//let themeButton = document.querySelector('.theme-button')
 
 
-let smena = () => body.classList.toggle('light-theme') & body.classList.toggle('dark-theme')
+//let smena = () => body.classList.toggle('light-theme') & body.classList.toggle('dark-theme')
 
-themeButton.addEventListener('click', smena)
+//themeButton.addEventListener('click', smena)
+
+const body = document.querySelector('.page')
+const themeButton = document.querySelector('.theme-button')
+
+const changeTheme = () => body.classList.toggle('light-theme') & body.classList.toggle('dark-theme')
+themeButton.addEventListener('click', changeTheme)
+
+const textInput = document.querySelector('.text')
+const buttonGo = document.querySelector('.go')
+
+let func = () => {
+    console.dir(textInput.value)
+    textInput.value = ''
+}
+
+buttonGo.addEventListener('click', func)

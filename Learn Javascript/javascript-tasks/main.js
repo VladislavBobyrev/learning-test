78b0e2d407edaec9459ce59dbfca5a5889c09e63
vladislav <perets1995@gmail.com>
2021-04-30 19:43:23 +0300

    console

--- Learn Javascript/javascript-tasks/main.js
+++ Learn Javascript/javascript-tasks/main.js
@@ -1,33 +1,42 @@
-let finalGrade = (exam, project) => {
-    let result = 0
-    exam > 90 ?? project > 10 ? result = 100 :
-    exam > 75 ?? project > 5 ? result = 90 :
-    exam > 50 ?? project > 2 ? result = 75 : 
-    result = 0
-    return result
-}
+//let finalGrade = (exam, project) => {
+//    let result = 0
+//    exam > 90 ?? project > 10 ? result = 100 :
+//    exam > 75 ?? project > 5 ? result = 90 :
+//    exam > 50 ?? project > 2 ? result = 75 : 
+//    result = 0
+//    return result
+//}
 
-console.log( finalGrade(70, 2))
+//console.log( finalGrade(70, 2))
 
-let example = (text) => {
-
-
-     let string = text.toUpperCase()
-     console.log(string == toUpperCase())
-     string.toLowerCase()
+//let example = (text) => {
 
 
-     return string
-}
+//     let string = text.toUpperCase()
+//     console.log(string == toUpperCase())
+//     string.toLowerCase()
 
-example('dsfl')
-//=========================ivan-petrishenko
-let pow = (a, b) => {
-  let result = 1
-}
-// пустая строка между логическими блоками
-if(1 < 2){
-  console.dir('step')
-} else {
-  console.dir( ('step no found') ) // gap beetwen вложенными вызовами
-}
+
+//     return string
+//}
+
+//example('dsfl')
+////=========================ivan-petrishenko
+//let pow = (a, b) => {
+//  let result = 1
+//}
+//// пустая строка между логическими блоками
+//if(1 < 2){
+//  console.dir('step')
+//} else {
+//  console.dir( ('step no found') ) // gap beetwen вложенными вызовами
+//}
+
+//const answer = []
+//answer[0] = prompt('What are you name?', 'Vladislav')
+//document.write(answer)
+
+//// cours ivan petrihenko
+
+//console.dir(document.body)
+console.dir(document.body.childNodes)

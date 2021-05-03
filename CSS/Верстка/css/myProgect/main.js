9a63f76347c5a32f5adab23b2d18a1e1d7a39b49
vladislav <perets1995@gmail.com>
2021-05-03 11:20:59 +0300

    function constructor

--- CSS/Верстка/css/myProgect/main.js
+++ CSS/Верстка/css/myProgect/main.js
@@ -1,7 +1,31 @@
-const btn = document.querySelector('.modal__close')
+const btnModal = document.querySelector('.modal')
+const btn = document.querySelector('.show')
+const btnClose = document.querySelector('.close')
+
+const open = () => {
+	btnModal.classList.add('a')
+	btnModal.classList.remove('modal')
+}
+btn.addEventListener('click', open)
 
 const closeum = () => {
-	btn.classList.remove('modal')
+	btnModal.classList.add('modal')
 }
 
-btn.addEventListener('click', closeum)
+btnClose.addEventListener('click', closeum)
+
+function User(name, id){
+	this.name = name
+	this.id = id
+	this.human = true
+	this.hello = function () {
+		console.log(`${name} Welcome to page, your years is ${id} million old `)
+	}
+}
+
+
+let Vlad = new User('Vlad drakul', 25)
+console.dir(Vlad)
+Vlad.hello()
+
+const obj 

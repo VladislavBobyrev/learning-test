5fbda25f15acd778c6c4ccc0d1890b95aa9d97c9
vladislav <perets1995@gmail.com>
2021-05-05 12:37:32 +0300

    constructor

--- Learn Javascript/javascript-tasks/main.js
+++ Learn Javascript/javascript-tasks/main.js
@@ -1,49 +1,10 @@
-//let finalGrade = (exam, project) => {
-//    let result = 0
-//    exam > 90 ?? project > 10 ? result = 100 :
-//    exam > 75 ?? project > 5 ? result = 90 :
-//    exam > 50 ?? project > 2 ? result = 75 : 
-//    result = 0
-//    return result
-//}
-
-//console.log( finalGrade(70, 2))
-
-//let example = (text) => {
-
-
-//     let string = text.toUpperCase()
-//     console.log(string == toUpperCase())
-//     string.toLowerCase()
-
-
-//     return string
-//}
-
-//example('dsfl')
-////=========================ivan-petrishenko
-//let pow = (a, b) => {
-//  let result = 1
-//}
-//// пустая строка между логическими блоками
-//if(1 < 2){
-//  console.dir('step')
-//} else {
-//  console.dir( ('step no found') ) // gap beetwen вложенными вызовами
-//}
-
-//const answer = []
-//answer[0] = prompt('What are you name?', 'Vladislav')
-//document.write(answer)
-
-//// cours ivan petrihenko
-
-//console.dir(document.body)
-console.dir(document.body.childNodes)
-console.dir(document.documentElement)
-console.dir(document.body.firstChild)
-console.dir(document.querySelector('.html'))
-
-setTimeout(function(){
-	alert('Hello Mashenka')
-}, 10000)
+class MenuCard {
+	// кончтруктор примимает в себя путь картинки фльт титулное название описание цену
+	constructor(src, alt, title, description, price){
+		this.src = src
+		this.alt = alt
+		this.title = title
+		this.description = description
+		this.price = price
+	}
+}

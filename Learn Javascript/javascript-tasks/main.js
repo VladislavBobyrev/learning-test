d77adfd2c110a44b934af749272df0fd6ec4077a
vladislav <perets1995@gmail.com>
2021-05-05 13:07:57 +0300

    bag

--- Learn Javascript/javascript-tasks/main.js
+++ Learn Javascript/javascript-tasks/main.js
@@ -1,12 +1,12 @@
 class MenuCard {
-	// кончтруктор примимает в себя путь картинки фльт титулное название описание цену
-	constructor(src, alt, title, description, price) {
+	constructor(src, alt, title, description, price, parentSelector) { // кончтруктор примимает в себя путь картинки фльт титулное название описание цену
 		this.src = src
 		this.alt = alt
 		this.title = title
 		this.description = description
 		this.price = price
-		this.transfer = this.transfer		// значение доллара с сервера
+		this.parent = document.querySelector(parentSelector)
+		this.transfer = transfer		// значение доллара с сервера
 		this.chandeToDollar()						// вызываем метод каторый переводит рубли в доллары
 	}
 
@@ -14,19 +14,29 @@ class MenuCard {
 		this.prise = this.price * this.transfer
 	}
 
-	render() {	
+	render() {
 		const element = document.createElement('div')
 		element.innerHTML = `
-		<div class="meny_item">
-		<img src="" alt="" srcset="">
-		<h3></h3>
-		<div class="meny_description"></div>
-		<div class="meny_deviver"></div>
-		<div class="meny_price">
-			<div class="meny_price__cost"></div>
-			<div class="meny_price__total"></div>			
-		</div>		
-	</div>
+			<div class="meny_item">
+			<img src=${this.src} alt=${this.alt}>
+			<h3>${this.title}</h3>
+			<div class="meny_description">${this.description}</div>
+			<div class="meny_deviver"></div>
+			<div class="meny_price">
+				<div class="meny_price__cost">${this.price}</div>
+				<div class="meny_price__total">${this.transfer}</div>			
+			</div>		
+			</div>
 		`
+		this.parent.append(element)
 	}
 }
+
+new MenuCard(
+	"./index.html",
+	'Boss Mary',
+	'Menu "FITNES"',
+	'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem aspernatur non unde porro nam neque quaerat doloribus, tempora totam quis suscipit ipsa itaque reprehenderit harum quos eos maiores culpa vero?',
+	9,
+	'.menu .container'
+).render()

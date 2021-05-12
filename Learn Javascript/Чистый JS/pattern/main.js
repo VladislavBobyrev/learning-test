611bd2ecb0c9bec7e7b9c529199b2b507d83197e
vladislav <perets1995@gmail.com>
2021-05-12 11:41:10 +0300

    Flag

--- Learn Javascript/Чистый JS/pattern/main.js
+++ Learn Javascript/Чистый JS/pattern/main.js
@@ -1,4 +1,8 @@
-const ansver = prompt('What are you name? ')
-const req = /n/
+//const ansver = prompt('What are you name? ')
+//const req = /\d/g
 
-console.log(ansver.search(req))
+////console.log(ansver.search(req))
+//console.log(ansver.match(req))
+
+const stri = 'my name is R2D2'
+console.log(stri.match(/\w\d\w\d/i))

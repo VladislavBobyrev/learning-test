347d452ebf5b880b6b6a64daa5e5b2e27a894618
vladislav <perets1995@gmail.com>
2021-06-21 10:22:22 +0300

    promis value

--- Learn Javascript/Чистый JS/promises/main.js
+++ Learn Javascript/Чистый JS/promises/main.js
@@ -8,3 +8,12 @@ prom.then((value) => {
     console.dir(value)
 })
 console.dir(prom)
+
+const promis = new Promise((res, rej) => {
+	res(() => console.log('hello Promises'))
+	rej(() => console.log('err reject'))
+}).then(value => {
+	console.log(value)
+})
+
+console.log(promis)

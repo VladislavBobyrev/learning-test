5a1417772bed23b21e0c6da7196b3f54f6f7a447
vladislav <perets1995@gmail.com>
2021-06-21 10:28:32 +0300

    recursia

--- Learn Javascript/Чистый JS/recursia/main.js
+++ Learn Javascript/Чистый JS/recursia/main.js
@@ -1,7 +1,15 @@
-function rec(x){
-    if(x >= 10) {
-        return
-    }
-    rec(x + 1)
+//function rec(x){
+//    if(x >= 10) {
+//        return
+//    }
+//    rec(x + 1)
+//}
+//console.dir( rec(4))
+
+function recurs (a){
+	if(a > 10) {
+		return
+	}
+return	a >=10 ? null : recurs(a + 1 )	
 }
-console.dir( rec(4))
+console.log(recurs(2))

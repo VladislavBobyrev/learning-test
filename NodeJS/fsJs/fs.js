80fd0eb720b650b5e0bafc06331aec337aa53949
vladislav <perets1995@gmail.com>
2021-06-25 18:29:17 +0300

    learn nodeJS

--- NodeJS/fsJs/fs.js
+++ NodeJS/fsJs/fs.js
@@ -3,12 +3,24 @@ const path = require('path')
 // module.exports = up
 
 const fs = require('fs')
+// сщздает новую папку joinом превразаем в строку путь и название новой папки а mldir создает ее спомощю ls module
+//fs.mkdir(path.join(__dirname, 'test'),(err) => {
+//if(err) {
+//	throw err
+//}
+//console.log(`folder compleet ))`)
+//})
 
-const fs = fs.mkdir(path.join(__dirname, test),(err) => {
-if(err) {
-	throw err
-}
-console.log(`folder compleet ))`)
+const filePath = path.join(__dirname, 'test.txt')
+// сщздает файл и перетерает 
+//fs.writeFile(filePath, 'Hello writeFile', err => {
+//	err ? () => {
+//		throw err
+//	} : console.log('file compleet')
+//})
+// обновляет файл и дабавляет новый контент
+fs.appendFile(filePath, 'Hello appendFile', err => {
+	err ? () => {
+		throw err
+	} : console.log('file compleet')
 })
-
-module.exports = fs

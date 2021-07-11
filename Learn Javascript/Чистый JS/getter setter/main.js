e93e74ff0ba3c736c5c75f688209da1e14a1783a
vladislav <perets1995@gmail.com>
2021-07-11 17:11:45 +0300

    setter

--- Learn Javascript/Чистый JS/getter setter/main.js
+++ Learn Javascript/Чистый JS/getter setter/main.js
@@ -4,6 +4,29 @@ let user = {
 
 	get fullneme() {
 		return ` ${this.name} ${this.sureName}`
+	},
+
+	set fullneme(value) {	
+		[ this.name, this.sureName] = value.split(' ')
 	}
 }
+
+user.fullneme = 'Mary Bobyreva'
 console.log( user.fullneme)
+
+
+function User (name, birtsday) {
+	this.name = name
+	this.birtsday = birtsday
+
+	Object.defineProperty(this, 'age', {
+		get() {
+			let todayYera = new Data().getFullYear()
+			return todayYera - this.birtsday.getFullYear()
+		}
+	})
+}
+
+let Vladislav = new User('Vladislav', new Data(1995,9,1))
+
+console.log(Vladislav.birtsday)

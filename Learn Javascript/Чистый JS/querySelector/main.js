cd4628dd37c8409c5e58331c6b3acc2aa320ea97
vladislav <perets1995@gmail.com>
2021-07-13 17:02:15 +0300

    DOM tree

---
+++ Learn Javascript/Чистый JS/querySelector/main.js
@@ -0,0 +1,9 @@
+const divs =  document.querySelector('.divs') // 1
+
+console.log(divs.nodeType)
+console.log(document.body.constructor.name)
+
+console.log(document.body instanceof EventTarget)
+
+console.log(document.body)
+console.dir(document.body)

43a426ab268f127fec9eb8d8dfed584d17cb717a
vladislav <perets1995@gmail.com>
2021-07-13 12:55:03 +0300

    побитовые операторы

---
+++ Learn Javascript/Чистый JS/побитовые операторы/main.js
@@ -0,0 +1,13 @@
+const ACCESS_ADMIN = 1;          // 00001
+const ACCESS_GOODS_EDIT = 2;   // 00010
+const ACCESS_GOODS_VIEW = 4;     // 00100
+const ACCESS_ARTICLE_EDIT = 8; // 01000
+const ACCESS_ARTICLE_VIEW = 16;  // 10000
+
+let guest = ACCESS_ARTICLE_VIEW | ACCESS_GOODS_VIEW
+let editor = guest | ACCESS_ARTICLE_EDIT | ACCESS_GOODS_EDIT; // 11110
+let admin = editor | ACCESS_ADMIN; // 11111
+
+console.log(guest)
+
+console.log(editor  & ACCESS_ADMIN)

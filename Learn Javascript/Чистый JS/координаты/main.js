b900be79b0a9540089d01056453f5c3547294ea1
vladislav <perets1995@gmail.com>
2021-07-14 10:43:36 +0300

    координаты

--- Learn Javascript/Чистый JS/координаты/main.js
+++ Learn Javascript/Чистый JS/координаты/main.js
@@ -1,8 +1,8 @@
 const body = document.querySelector('body')
 console.log(body.getBoundingClientRect())
 
-//let elem = document.elementFromPoint(x, y);
-//console.log(elem)
+let elem = document.elementFromPoint(1, 5);
+console.log(elem)
 // ширина экрана
 console.log( document.documentElement.clientWidth / 2)
 
@@ -15,10 +15,16 @@ const createMessage = (elem, html) => {
 	let cords = elem.getBoundingClientRect()
 
 	message.innerHTML = html
-	console.log(cords)
+	console.log(cords.top)
+	console.log(pageXOffset)
 
 	return message
 }
 
 let el = createMessage(blokc, 'hello')
 blokc.append(el)
+
+let bl = blokc.getBoundingClientRect()
+console.log(bl.top)
+console.log(bl.left)
+

82a22df9d3ac93ecbbe6276d41b2861452163b90
vladislav <perets1995@gmail.com>
2021-07-14 13:15:23 +0300

    scroll

--- Learn Javascript/Чистый JS/cобытия/main.js
+++ Learn Javascript/Чистый JS/cобытия/main.js
@@ -12,17 +12,34 @@
 
 
 const body = document.querySelector('body')
+//console.log( document.documentElement.getBoundingClientRect().top = 0)
+//console.log(document.documentElement.getBoundingClientRect().bottom = 2000)
+
+function populate() {
+  while(true) {
+    // нижняя граница документа
+    let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+
+    // если пользователь прокрутил достаточно далеко (< 100px до конца)
+    if (windowRelativeBottom < document.documentElement.clientHeight + 100) {
+      // добавим больше данных
+      document.body.insertAdjacentHTML("beforeend", `<p>Дата: ${new Date()}</p>`);
+    }
+  }
+}
+
+
 //const fn = () => {
 //	console.log('mouseDovn')
 //}
 //body.addEventListener('mousedown', fn)
 
 
-const fnmouse = (evt) => {
-	console.log(evt.clientX)
-	console.log(evt.clientY)
-}
-body.addEventListener('mousemove', fnmouse)
+//const fnmouse = (evt) => {
+//	console.log(evt.clientX)
+//	console.log(evt.clientY)
+//}
+//body.addEventListener('mousemove', fnmouse)
 // теперь у боди есть наше событие
 //body.dispatchEvent(evt)
 

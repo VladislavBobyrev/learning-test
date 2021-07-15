66227a7f73a6939fa5e621a43338029630e50a2f
vladislav <perets1995@gmail.com>
2021-07-15 10:52:30 +0300

    binary cide

---
+++ Learn Javascript/Чистый JS/бинарные даныне/main.js
@@ -0,0 +1,15 @@
+let arr8 = new Uint8Array(4)
+// перезаписали в 16 битовый вариант
+let arr16 = new Uint16Array(arr8)
+
+console.log(arr8.byteLength)
+console.log(arr8.BYTES_PER_ELEMENT)
+
+// ================= DataView
+let buffer = new Uint8Array([255, 255, 255, 255]).buffer
+console.log(buffer)
+
+let dataView = new DataView(buffer)
+console.log( dataView.getUint8(0))
+console.log( dataView.getUint16(0))
+console.log( dataView.getUint32(0))

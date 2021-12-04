057a0ad79d4bf92a318e204a0a28ff6b32ee6ac0
vladislav <perets1995@gmail.com>
2021-12-04 13:27:03 +0300

    create VUE 3

---
+++ VUE/button/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>this my first counter</h1>
+    <button @click='counter++'>num :{{counter}}</button>
+    <button @click='counter--'>num :{{counter}}</button>
+  </div>
+</template>

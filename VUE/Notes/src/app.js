37b650defede0b7eff544a8a0203397fabf21861
Vladislav.Bobyrev <perets1995@gmail.com>
2021-12-27 21:34:09 +0300

    v-cloak test

--- VUE/Notes/src/app.js
+++ VUE/Notes/src/app.js
@@ -1,6 +1,30 @@
-Vue.createApp({
-  data: () => ({
-    'title': 'Hello vue 3',
-    "counter": 0
-  })
-}).mount("#app")
+const App = {
+  data()
+  {
+    return {
+      title: 'Hello vue 3',
+      plaсeholderString: 'введите текст заметки',
+      inputValue: '',
+      notes: [
+        "Заметка 1"
+      ]
+    }
+
+  },
+  methods: {
+    inputChengeHandler(event)
+    {
+      this.inputValue = event.target.value
+     
+    },
+    addNewNotes()
+    {
+      console.log(this.notes) 
+      this.notes.push(this.inputValue)
+    }
+  }
+
+}
+
+const app = Vue.createApp(App)
+app.mount('#app')

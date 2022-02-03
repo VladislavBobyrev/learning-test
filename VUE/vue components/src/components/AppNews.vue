c5a441ad3cfe31eb4acabe71749afb5f19f00399
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-02-03 19:21:30 +0300

    Add files via upload

---
+++ VUE/vue components/src/components/AppNews.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card" >
+    <h2>Aктуальные новости: {{item}}</h2>
+    <button class="btn" @click="isOpen = !isOpen">open</button>
+    <div class="card__text" v-if="isOpen">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, rerum nisi tenetur deleniti, recusandae delectus adipisci harum qui vero porro ex illum quam nemo ducimus impedit necessitatibus nam quaerat dolorum.</div>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return {
+      item: 'Заголовок',
+      isOpen:false
+    }
+  }
+}
+</script>

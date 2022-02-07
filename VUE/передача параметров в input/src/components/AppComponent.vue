d64467e9eecf4d09fcc6c8fdf6f437e144e89be1
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-02-07 18:27:00 +0300

    Add files via upload

---
+++ VUE/передача параметров в input/src/components/AppComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <label for="sity" >are your sity?</label>
+  <input id="sity" name="sity" list="list-sity">
+
+  <datalist id="list-sity" v-for="item in sity">
+    <option value="{{item}}"></option>  
+  </datalist>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sity: ['moscow', 'america', 'france', 'egypt', 'london', 'germany', 'belarus', 'ukrain']
+    }
+  },
+}
+</script>

df2476685efb4f66cdfe2f9648324895819451a0
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-02-09 18:38:27 +0300

    Add files via upload

---
+++ VUE/передача свойств родителю/src/components/AppCard.vue
@@ -0,0 +1,46 @@
+<template>
+
+<aside class="card" 
+v-for="item in infoCard"
+@itter="isItter"
+>
+  <div class="card_icon"></div>
+  <section class="card_body">
+    <div class="card_title">{{item.title}}</div>
+    <div class="card_text">{{item.text}}</div>
+  </section>
+</aside>
+
+</template>
+
+<script>
+export default {
+  emits: ['id'],
+  methods:{
+    itter(){
+      this.id++
+      this.$emit('itter', { id: this.id})
+    }
+  },
+  data(){
+    return{
+     infoCard: [
+       {
+         title: 'Title 1',
+         text: 'text text texttexttext text text '
+       },
+       {
+         title: 'Title 2',
+         text: 'text text texttexttext text text '
+       },
+       {
+         title: 'Title 3',
+         text: 'text text texttexttext text text '
+       },
+
+     ],
+     id: 1,
+    }
+  }
+}
+</script>

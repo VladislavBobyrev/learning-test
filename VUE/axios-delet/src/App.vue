9abf0fbccaf563f40c6467849de56c3be399051c
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-02-24 22:23:02 +0300

    Add files via upload

---
+++ VUE/axios-delet/src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <form class="container" @submit.prevent="createPerson">
+    <div class="card">
+      <h2>Work in REST api</h2>
+
+      <div class="form-control">
+        <label for="name"></label>
+        <input 
+          type="text"
+          id="name"
+          v-model.trim="name"  
+          >
+      </div>
+
+      <button class="btn primary"
+        :disabled="name.length == 0"
+        @click="createPerson"
+        >Save DataBase</button>
+
+    </div>
+  </form>
+
+   <app-people-list
+   :people="people"
+   @load="loadPeople"
+   @remove="removePeople"
+   ></app-people-list>
+</template>
+
+<script>
+import AppPeopleList from './components/AppPeopleList.vue';
+import axios from 'axios'
+
+export default {
+  props: ['load', 'remove'],
+  data() {  
+    return{
+      name: '',
+      people: [],
+    }
+  },
+  mounted(){
+    this.loadPeople()
+  },
+  methods:{
+  /**
+   Метод принимает данные из формы для загрузки в БД
+  * @method createPerson
+  * @param    {string} Name    Имя клиента
+  * @async
+  * */
+   async createPerson(){
+      // https://vue3-rest-api-default-rtdb.firebaseio.com/people.json
+      // this.name
+      const respons = await fetch('https://vue3-rest-api-default-rtdb.firebaseio.com/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body:  JSON.stringify({
+          firstName: this.name
+        })        
+      })
+      const fireBase = await respons.json()
+      this.people.push({
+        firstName:this.name,
+        id: fireBase.name,
+      })
+      this.name = ''
+    },
+    /**метод загружает список из БД 
+     * @method loadPeople
+     * @async
+    */
+    async loadPeople(){
+      const {data} =await axios.get('https://vue3-rest-api-default-rtdb.firebaseio.com/people.json')
+     this.people = Object.keys(data).map(key => {
+       return {
+       id: key,
+       ...data[key]
+     }
+   })
+   },
+   /**метод удаляет элумент по id
+    * @method 
+    * @async
+    * @param {string} id
+    */
+   async removePeople(id){
+     await axios.delete(`https://vue3-rest-api-default-rtdb.firebaseio.com/people/${id}.json`)
+     this.people = this.people.filter(person => person.id !== id)
+   }
+  },
+  components:{
+    AppPeopleList
+  }
+}
+</script>
+
+<style>
+
+</style>

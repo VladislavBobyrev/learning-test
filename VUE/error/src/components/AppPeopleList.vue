52721dcdabd0cfb88fc1d04b180315479830bfe0
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-02-26 20:32:12 +0300

    Add files via upload

---
+++ VUE/error/src/components/AppPeopleList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul 
+  class="card"
+  v-if="people.length !== 0">
+    <li class="card jc-sb" 
+    v-for="person in people"
+    :key="person.id"
+    >
+      {{person.firstName}}
+      <button class="btn danger" @click="$emit('remove', person.id) ">delet</button>
+    </li>
+  </ul>
+
+  <div class="card center" v-else>
+    <h3>person is not ready</h3>
+    <button c
+      lass="btn"
+      @click="$emit('load')"
+      >
+      download list
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits:['load', 'remove'],
+  props: ['people']
+}
+</script>
+
+<style>
+
+</style>

93568f9650bc3452ee78cf29b6a7b348f08a88c0
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-03-06 10:38:49 +0300

    Add files via upload

---
+++ VUE/vue directive/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h1>Directive</h1>
+    </div>
+    <div class="card">
+      <h3>Active input default</h3>
+      <input type="text" id="message" v-focus>
+      <label for="message">Your messsage</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+directives:{
+  focus: {
+    mounted(el){
+      el.focus()
+    }
+  }
+}
+}
+</script>
+
+<style>
+
+</style>

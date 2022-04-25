d9c90ecb796389e645cdbcb4a6541d747b1b002e
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-04-25 21:37:40 +0300

    Add files via upload

---
+++ vueX/increment/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container with-nav">
+    <div class="card">
+      <h1>Счетчик на VueX </h1>
+      <h2>Счетч {{$store.state.count}}</h2>
+      <button @click="increment">increment</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from './TheNavbar'
+export default {
+  components: {TheNavbar},
+  methods: {
+    increment(){
+      this.$store.state.count++
+    }
+  }
+}
+</script>
---
+++ vueX/increment/src/TheNavbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="navbar">
+    <span>Vuex</span>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
